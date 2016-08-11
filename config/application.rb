d7af8370ca12b0d03506680276152d260eca7a04
jcerexhe <jcerexhe@gmail.com>
2016-08-11 14:31:08 +1000

    Updated default timezone to Sydney

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module Cfastarterapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Sydney'
+    config.active_record.default_timezone = :local
   end
 end
